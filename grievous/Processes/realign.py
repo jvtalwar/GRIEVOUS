7bd124156614b8e2a61f6d9de4234f072c239f11
James Talwar <jtalwar@nrnb-login-01.cm.cluster>
2023-11-08 18:46:25 -0800

    Clarified comment language

--- grievous/Processes/realign.py
+++ grievous/Processes/realign.py
@@ -13,12 +13,12 @@ logger = logging.getLogger(__name__)
 '''
 About: Helper method to create a new grievous database/dictionary directory.
 
-Input(s): newDictPath: String corresponding to the directory where empty database/dictionary records will be created to be updated.
+Input(s):  1) newDictPath: String corresponding to the directory where empty database/dictionary records will be created to be updated.
+           2) dictStorage: String corresponding to database storage method. Default: 'parquet'
 Output(s): None
 '''
 def _Create_Dictionary(newDictPath, dictStorage = "parquet"):
-    # assert not os.path.isdir(newDictPath), f"Dictionary already exists at {newDictPath}" #<--- collisions might occur in parallel, change to if, else and exist_ok = True below
-    if os.path.isdir(newDictPath):
+    if os.path.isdir(newDictPath): #chromosome-level paralleleization - if database is in process of (or has finished) being created don't need to re-create/overwrite db
         return None
 
     #Create new dictionary folder
@@ -29,11 +29,11 @@ def _Create_Dictionary(newDictPath, dictStorage = "parquet"):
     chromosomes = [str(i) for i in range(1,23)] + ["X", "Y", "MT"] #Grievous standard for chromosomes
 
     for chromosome in chromosomes:
-        # Record aligned files. Keeping at chromosome level prevents record overwrite issues/collisions when parallelized 
+        # all the files aligned to given database 
         with open(os.path.join(newDictPath, f"Records/CHR_{chromosome}.aligned.record.txt"), "a") as alignedRecord: 
             pass
 
-        #all the files which were used to update the dictionary. If --shutoff_dict_update is never used then is equivalent to aligned
+        # all the files which were used to update the given database. If --shutoff_dict_update is never used then is equivalent to aligned
         with open(os.path.join(newDictPath, f"Records/CHR_{chromosome}.updated.record.txt"), "a") as updatedRecord: 
             pass
 
@@ -63,7 +63,7 @@ def _Check_Dictionary(dbAlias):
     if dbAlias not in grievousDictAliases:
         if dbAlias is None: #Handle automatically when only one dictionary standard exists.
             assert len(grievousDictAliases) == 1, """--database not provided and either more than one database exists or none have been created. 
-            Provide a current database/dictionary alias or provide a new one to create new empty database. Database aliases can be found with grievous list_dbs."""
+            Provide a current database/dictionary alias or provide a new one to create a new empty database. Database aliases can be found with grievous list_dbs."""
 
             grievousDictDir = os.path.join(grievousDir, grievousDictAliases[0])
 
@@ -86,14 +86,14 @@ def _Check_Dictionary(dbAlias):
 
 
 '''
-About: This method performs Grievous realignment, by creating a PVAR/SSF object and then calling the instance methods. For full details of these methods see
+About: This method performs grievous realignment, by creating a PVAR/SSF object and then calling the instance methods. For full details of these methods see
 package GenomicObjects.
 
 Input(s): 1) file: String corresponding to the path of the file which the user is attempting to realign.
           2) dictionary: String corresponding to the dictionary/database to which the user is attempting to realign. 
           3) write_path: String corresponding to the directory where the user wants to write realignment files.
           4) mapping: String corresponding to the file path registering all mandatory non-grievous standard column names to the grievous standard.
-          5) file_type: String in {'pvar', 'ssf'} corresponding to the desire genomic file type to be realigned.
+          5) file_type: String in {'pvar', 'ssf'} corresponding to the desired genomic file type to be realigned.
           6) return_all: Boolean corresponding to whether to return all columns of the underlying file, or the grievous subset.
           7) comment_characters: List of all comment characters corresponding to lines to skip at the beginning of a genomic file.
           8) shutoff_dict_update: Boolean corresponding to whether to skip the dictionary update step. If there is ever any ambiguity 
@@ -127,7 +127,7 @@ def Realign(file, dictionary, write_path, mapping, file_type, return_all, commen
     else:
         gFile = SSF(filePath = file, mappingPath = mapping, commentCharsToSkip = comment_characters)
 
-    #Identify/Make necessary dictionary directory and load chromosome-level dictionary
+    #Identify/Make necessary grievous database/dictionary directory and load chromosome-level database/dictionary
     grievousDictionaryPath = _Check_Dictionary(dictionary)
     dictionaryExtension = ""
 
@@ -138,8 +138,8 @@ def Realign(file, dictionary, write_path, mapping, file_type, return_all, commen
         dictionaryExtension = "parquet"
 
     except:
-        logger.info("parquet chromosome dictionary not found... Trying tsv")
-        assert f"CHR_{gFile.Chrom}.tsv" in os.listdir(grievousDictionaryPath), f"No valid dictionary file found in dictionary {dictionary} for chromosome {gFile.Chrom}. Exiting..."
+        logger.info("parquet chromosome database not found... Trying tsv")
+        assert f"CHR_{gFile.Chrom}.tsv" in os.listdir(grievousDictionaryPath), f"No valid chromosomal database file found in grievous database {dictionary} for chromosome {gFile.Chrom}. Exiting..."
         chromosomeDictionary = pd.read_csv(os.path.join(grievousDictionaryPath, f"CHR_{gFile.Chrom}.tsv"), sep = "\t", index_col = 0, dtype={'CHR': str})
 
         dictionaryExtension = "tsv"
@@ -149,22 +149,23 @@ def Realign(file, dictionary, write_path, mapping, file_type, return_all, commen
     if write_path is None:
         fileParentDir = os.path.dirname(file)
         write_path = os.path.join(fileParentDir, "GrievousAlignedFiles")
+        logger.warning(f"No write_path provided. Writing aligned files to a new directory {write_path}")
         os.makedirs(write_path, exist_ok = True) 
 
     else:
         if (os.path.exists(os.path.join(write_path, "GRIEVOUS_Formatted", f"FormattedAndAlignedPvar_CHR{gFile.Chrom}.tsv"))) or (os.path.exists(os.path.join(write_path, "GRIEVOUS_Formatted", f"FormattedAndAlignedSSF_CHR{gFile.Chrom}.tsv"))):
-            logger.warning(f"Previous GRIEVOUS Formatted file detected in write_path {write_path}. If this directory was used for a previous realignment, current files will be overwritten.") 
+            logger.warning(f"Previous GRIEVOUS_Formatted file detected in write_path {write_path}. If this directory was used for a previous realignment, current files will be overwritten.") 
 
     # Make reports, reorientation, and greivousFormatted folders for output files
-    os.makedirs(os.path.join(write_path, "Reports") , exist_ok = True)
-    os.makedirs(os.path.join(write_path, "GRIEVOUS_Formatted") , exist_ok = True)
+    os.makedirs(os.path.join(write_path, "Reports"), exist_ok = True)
+    os.makedirs(os.path.join(write_path, "GRIEVOUS_Formatted"), exist_ok = True)
 
     if file_type == "pvar":       
-        os.makedirs(os.path.join(write_path, "Reorientation") , exist_ok = True)
+        os.makedirs(os.path.join(write_path, "Reorientation"), exist_ok = True)
 
 
 
-    #Implement Realignment:
+    #Implement Realignment
 
     #Employ Clean:
     gFile.Clean()
@@ -176,7 +177,7 @@ def Realign(file, dictionary, write_path, mapping, file_type, return_all, commen
     gFile.Align(write_path)
 
     if not shutoff_dict_update: 
-        #Update dictionary     
+        #Update grievous database/dictionary     
         gFile.UpdateDict(chromosomeDictionary,  grievousDictionaryPath, dictionaryExtension)
 
         #Update .updated.record file
@@ -184,10 +185,10 @@ def Realign(file, dictionary, write_path, mapping, file_type, return_all, commen
             updateRecord.write(file + "\n")
         
 
-    #Write Aligned Files:
+    #Write realigned files:
     gFile.Write(write_path, return_all)
 
-    #update .aligned.record file
+    #update .aligned.record file for the given grievous database
     with open(os.path.join(grievousDictionaryPath, f"Records/CHR_{gFile.Chrom}.aligned.record.txt"), "a") as alignedRecord:
         alignedRecord.write(file + "\n")
     
